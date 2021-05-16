968cf7b628cedb0eb4f930e387713f0c0ef194fe
Garbim <garbim7@gmail.com>
2021-05-16 13:59:10 -0300

    rota controller e title

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,8 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $data['name'] = 'Inicio';
+        $data['title'] = 'Home';
+        return view('home', $data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app', ['title'=> $title ])
 <!-- This example requires Tailwind CSS v2.0+ -->
 <div class="relative bg-black overflow-hidden">
   <div class="max-w-12xl mx-auto">
@@ -11,7 +11,7 @@
         <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
           <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
             <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-              <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Home</a>
+              <a href="#" class="font-medium text-gray-500 hover:text-gray-900">{{ $name }}</a>
 
               <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Routes</a>
 
